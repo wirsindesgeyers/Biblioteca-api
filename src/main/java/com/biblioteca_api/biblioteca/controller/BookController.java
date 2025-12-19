803793095dae7ad76f6af639e63ec66e238c5458
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2025-12-19 15:00:45 -0400

    criação de bookcontroller (/api/books)

---
+++ src/main/java/com/biblioteca_api/biblioteca/controller/BookController.java
@@ -0,0 +1,16 @@
+package com.biblioteca_api.biblioteca.controller;
+
+
+import com.biblioteca_api.biblioteca.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+}
